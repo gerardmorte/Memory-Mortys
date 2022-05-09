cf7636a62007a4d8027382d22d4a9a9a8140a2fb
Gerard Morte <gerardmorte@gmail.com>
2022-05-09 15:50:23 +0200

    k.i.s.s count.down component

--- src/app/count-down/count-down.component.ts
+++ src/app/count-down/count-down.component.ts
@@ -7,6 +7,7 @@ import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 })
 export class CountDownComponent implements OnInit {
   seconds: number = 59;
+  showSeconds: string = 'START!';
 
   @Input() getIntentos: number = 0;
   @Input() getParejasOk: number = 0;
@@ -15,29 +16,13 @@ export class CountDownComponent implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    const getCountDown = document.getElementById('countDown')!;
-    getCountDown.innerHTML = 'START!';
-    this.seconds = 59;
     const temporizador = setInterval(() => {
-      if (this.getParejasOk == this.getArrayLength) {
-        getCountDown.innerHTML = 'WIN!';
-      } else if (this.seconds == -1) {
+      if (this.seconds == -1) {
         clearInterval(temporizador);
-        getCountDown.innerHTML = 'LOSE!';
-        //
-        //ENVIAR OUTPUT BOOLEAN A INFO-GAME-COMPONENT, OUTPUT DE INFO-GAME A BOARD-GAME?
-      //   const getCards = //getAllCards?
-      //   this.elementRef.nativeElement.getElementsByClassName('card');
-      // for (let index = 0; index < getCards.length; index++) {
-      //   //getCards[index].style.pointerEvents = 'auto';
-      //   this.renderer.setStyle(getCards[index], 'pointerEvents', 'none');
-      // }
-
-        //
       } else if (this.seconds < 10) {
-        getCountDown.innerHTML = '00:0' + this.seconds--;
+        this.showSeconds = '00:0' + this.seconds--;
       } else {
-        getCountDown.innerHTML = '00:' + this.seconds--;
+        this.showSeconds = '00:' + this.seconds--;
       }
     }, 1000);
   }
