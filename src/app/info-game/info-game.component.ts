f7a5d5cd7863c4255f81b747560f73888fea9e04
Gerard Morte <gerardmorte@gmail.com>
2022-05-09 13:58:26 +0200

    add ngIf on info-game component to show play, win or lose

--- src/app/info-game/info-game.component.ts
+++ src/app/info-game/info-game.component.ts
@@ -1,19 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info-game',
   templateUrl: './info-game.component.html',
-  styleUrls: ['./info-game.component.css']
+  styleUrls: ['./info-game.component.css'],
 })
 export class InfoGameComponent implements OnInit {
-
   @Input() getIntentos: number = 0;
   @Input() getParejasOk: number = 0;
   @Input() getArrayLength: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
